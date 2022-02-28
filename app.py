e31ee708c07f76124669a6c8b12e636bc652f88d
James Chen <james.chen@publicissapient.com>
2022-02-28 10:03:06 -0500

    f

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def events():
 @app.route('/')
 @cross_origin()
 def serve():
-    return send_from_directory(app.static_folder, "index.html")
+    return "<p>Hello</p>"
     
 if __name__ == '__main__':
     app.run()
