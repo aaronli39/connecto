1961bd587215023c3e93008e81b674beca7aa1db
James Chen <james.chen@publicissapient.com>
2022-02-28 00:30:08 -0500

    Fixing routing error

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 import requests
 import json
 from flask_cors import CORS
-app = Flask(__name__, static_url_path="", static_folder="../")
+app = Flask(__name__, static_url_path="", static_folder="client/build")
 CORS(app)
 @app.route("/events")
 def events():
