a627757015781fdd1b276e2e2cf8a6f0ce951718
Marco Raigoza <mraigoza48@gmail.com>
2022-04-20 01:22:37 -0400

    fixed upload

--- src/views/ProfileScreen.js
+++ src/views/ProfileScreen.js
@@ -1,18 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
-	View,
-	ScrollView,
-	Text,
-	TouchableOpacity,
-	ImageBackground,
-	TextInput,
-	StyleSheet,
-	FlatList,
-	Dimensions,
+  View,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  TextInput,
+  StyleSheet,
+  FlatList,
+  Dimensions,
 } from "react-native";
 import { Button, useTheme, withTheme } from "react-native-paper";
 import Constants from "expo-constants";
-import { useState } from "react";
 import axios from "axios";
 import UploadImage from "./UploadImage";
 import Ionicons from "react-native-vector-icons/Ionicons";
@@ -24,245 +23,276 @@ import { getDatabase, ref, onValue, set } from "firebase/database";
 // Import the functions you need from the SDKs you need
 import { app } from "./FirebaseInitialize";
 import {
-	getFirestore,
-	setDoc,
-	doc,
-	collection,
-	getDocs,
-	getDoc,
-	DocumentSnapshot,
+  getFirestore,
+  updateDoc,
+  doc,
+  collection,
+  getDocs,
+  getDoc,
+  DocumentSnapshot,
 } from "firebase/firestore";
 
 // THIS IS JUST A TEST FILE FOR NAVIGATION, PLEASE DELETE OR REPLACE THIS COMPONENT!
 const ProfileScreen = ({ navigation }) => {
-	// Initialize Firebase
-	const firestore = getFirestore(app);
-	const [firstName, setFirstName] = useState("");
-	const [lastName, setLastName] = useState("");
-	const [bio, setBio] = useState("");
-	const [phone, setPhone] = useState("");
-	const [email, setEmail] = useState("");
-	const [country, setCountry] = useState("");
-	const [city, setCity] = useState("");
-	const [profile, setProfile] = useState("");
-	//Collection Ref
-	const docRef = doc(firestore, "users", "DdRPo2lJfFbBcqkzAhXz");
-	getDoc(docRef)
-		.then((doc) => {
-			if (doc.exists) {
-				console.log("Document Data:", doc.data().FirstName);
-				let data = doc.data();
-				setFirstName(data.FirstName);
-				setLastName(data.LastName);
-				setBio(data.Biography);
-				setPhone(data.Phone);
-				setEmail(data.Email);
-				setCountry(data.Country);
-				setCity(data.City);
-			} else {
-				console.log("No such document");
-			}
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-	//const biography = profileData.getString("Biography");
-	//const city = profileData.getString("City");
-	//const country = document.getString("Country");
-	//const first = profileData.getString("FirstName");
-	//const last = profileData.getString("LastName");
-	//const phone = document.getString("Phone");
-	//const profileImage = document.getString("ProfileImage");
-	const { width } = Dimensions.get("window");
-	const SPACING = 10;
-	const THUMB_SIZE = 80;
-	const DATA = [
-		{ image: <UploadImageList list_num={0} />, id: "1" },
-		{ image: <UploadImageList list_num={1} />, id: "2" },
-		{ image: <UploadImageList list_num={2} />, id: "3" },
-		{ image: <UploadImageList list_num={3} />, id: "4" },
-		{ image: <UploadImageList list_num={4} />, id: "5" },
-		{ image: <UploadImageList list_num={5} />, id: "6" },
-	];
-	const { colors } = useTheme();
-	return (
-		<ScrollView
-			style={styles.container}
-			contentContainerStyle={{
-				marginTop: 50,
-				paddingBottom: 60,
-				marginLeft: 10,
-				marginRight: 10,
-				justifyContent: "center",
-				alignItems: "center",
-			}}
-		>
-			<Text style={{ alignSelf: "center", fontWeight: "bold" }}>
-				Hello, {firstName} {lastName}!
-			</Text>
-			<UploadImage style={{ alignSelf: "center" }} />
-			<View style={styles.action}>
-				<Feather name="info" color={colors.text} size={20} />
-				<TextInput
-					placeholder={bio}
-					placeholderTextColor="#666666"
-					autoCorrect={false}
-					style={[
-						styles.textInput,
-						{
-							color: colors.text,
-						},
-					]}
-				/>
-			</View>
-			<View style={styles.action}>
-				<FontAwesome name="user-o" color={colors.text} size={20} />
-				<TextInput
-					placeholder={firstName + " " + lastName}
-					placeholderTextColor="#666666"
-					autoCorrect={false}
-					style={[
-						styles.textInput,
-						{
-							color: colors.text,
-						},
-					]}
-				/>
-			</View>
+  // Initialize Firebase
+  const firestore = getFirestore(app);
 
-			<View style={styles.action}>
-				<Feather name="phone" color={colors.text} size={20} />
-				<TextInput
-					placeholder={phone}
-					placeholderTextColor="#666666"
-					keyboardType="number-pad"
-					autoCorrect={false}
-					style={[
-						styles.textInput,
-						{
-							color: colors.text,
-						},
-					]}
-				/>
-			</View>
-			<View style={styles.action}>
-				<FontAwesome name="envelope-o" color={colors.text} size={20} />
-				<TextInput
-					placeholder={email}
-					placeholderTextColor="#666666"
-					keyboardType="email-address"
-					autoCorrect={false}
-					style={[
-						styles.textInput,
-						{
-							color: colors.text,
-						},
-					]}
-				/>
-			</View>
-			<View style={styles.action}>
-				<FontAwesome name="globe" color={colors.text} size={20} />
-				<TextInput
-					placeholder={country}
-					placeholderTextColor="#666666"
-					autoCorrect={false}
-					style={[
-						styles.textInput,
-						{
-							color: colors.text,
-						},
-					]}
-				/>
-			</View>
-			<View style={styles.action}>
-				<Icon name="map-marker-outline" color={colors.text} size={20} />
-				<TextInput
-					placeholder={city}
-					placeholderTextColor="#666666"
-					autoCorrect={false}
-					style={[
-						styles.textInput,
-						{
-							color: colors.text,
-						},
-					]}
-				/>
-			</View>
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [bio, setBio] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  //Collection Ref
+  const docRef = doc(firestore, "users", "DdRPo2lJfFbBcqkzAhXz");
 
-			<FlatList
-				horizontal={true}
-				data={DATA}
-				style={{ marginTop: "5%", marginBottom: "5%" }}
-				showsHorizontalScrollIndicator={false}
-				contentContainerStyle={{
-					paddingHorizontal: SPACING,
-				}}
-				keyExtractor={(item) => item.id}
-				renderItem={({ item, index }) => (
-					<TouchableOpacity activeOpacity={0.9}>
-						<UploadImageList
-							list_num={index}
-							style={{
-								width: THUMB_SIZE,
-								height: THUMB_SIZE,
-								marginRight: SPACING,
-								borderRadius: 16,
-								//borderWidth: index === indexSelected ? 4 : 0.75,
-								//borderColor: index === indexSelected ? 'orange' : 'white'
-							}}
-						/>
-					</TouchableOpacity>
-				)}
-			/>
+  useEffect(() => {
+    getDoc(docRef)
+      .then((doc) => {
+        if (doc.exists) {
+          console.log("Document Data:", doc.data().FirstName);
+          let data = doc.data();
+          setFirstName(data.FirstName);
+          setLastName(data.LastName);
+          setBio(data.Biography);
+          setPhone(data.Phone);
+          setEmail(data.Email);
+          setCountry(data.Country);
+          setCity(data.City);
+        } else {
+          console.log("No such document");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-			<TouchableOpacity style={styles.commandButton} onPress={() => {}}>
-				<Text style={styles.panelButtonTitle}>Update Profile</Text>
-			</TouchableOpacity>
-		</ScrollView>
-	);
+  const { width } = Dimensions.get("window");
+  const SPACING = 10;
+  const THUMB_SIZE = 80;
+  const DATA = [
+    { image: <UploadImageList list_num={0} />, id: "1" },
+    { image: <UploadImageList list_num={1} />, id: "2" },
+    { image: <UploadImageList list_num={2} />, id: "3" },
+    { image: <UploadImageList list_num={3} />, id: "4" },
+    { image: <UploadImageList list_num={4} />, id: "5" },
+    { image: <UploadImageList list_num={5} />, id: "6" },
+  ];
+  const { colors } = useTheme();
+
+  const sumbitProfile = async () => {
+    console.log(
+      "Submit",
+      firstName,
+      lastName,
+      bio,
+      phone,
+      email,
+      country,
+      city
+    );
+
+    // const docSnap = await getDoc(docRef);
+    const docData = {
+      "FirstName": firstName,
+      "LastName": lastName,
+      "Biography": bio,
+      "Phone": phone,
+	  "Email": email,
+      "Country": country,
+      "City": city,
+    };
+    console.log(docData);
+    await updateDoc(docRef, docData);
+  };
+
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={{
+        marginTop: 50,
+        paddingBottom: 60,
+        marginLeft: 10,
+        marginRight: 10,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ alignSelf: "center", fontWeight: "bold" }}>
+        Hello, {firstName} {lastName}!
+      </Text>
+      <UploadImage style={{ alignSelf: "center" }} />
+      <View style={styles.action}>
+        <Feather name="info" color={colors.text} size={20} />
+        <TextInput
+          value={bio}
+          onChangeText={setBio}
+          placeholderTextColor="#666666"
+          autoCorrect={false}
+          style={[
+            styles.textInput,
+            {
+              color: colors.text,
+            },
+          ]}
+        />
+      </View>
+      <View style={styles.action}>
+        <FontAwesome name="user-o" color={colors.text} size={20} />
+        <TextInput
+          value={firstName + " " + lastName}
+          onChangeText={setFirstName}
+          placeholderTextColor="#666666"
+          autoCorrect={false}
+          style={[
+            styles.textInput,
+            {
+              color: colors.text,
+            },
+          ]}
+        />
+      </View>
+
+      <View style={styles.action}>
+        <Feather name="phone" color={colors.text} size={20} />
+        <TextInput
+          value={phone}
+          onChangeText={setPhone}
+          placeholderTextColor="#666666"
+          keyboardType="number-pad"
+          autoCorrect={false}
+          style={[
+            styles.textInput,
+            {
+              color: colors.text,
+            },
+          ]}
+        />
+      </View>
+      <View style={styles.action}>
+        <FontAwesome name="envelope-o" color={colors.text} size={20} />
+        <TextInput
+          value={email}
+          onChangeText={setEmail}
+          placeholderTextColor="#666666"
+          keyboardType="email-address"
+          autoCorrect={false}
+          style={[
+            styles.textInput,
+            {
+              color: colors.text,
+            },
+          ]}
+        />
+      </View>
+      <View style={styles.action}>
+        <FontAwesome name="globe" color={colors.text} size={20} />
+        <TextInput
+          value={country}
+          onChangeText={setCountry}
+          placeholderTextColor="#666666"
+          autoCorrect={false}
+          style={[
+            styles.textInput,
+            {
+              color: colors.text,
+            },
+          ]}
+        />
+      </View>
+      <View style={styles.action}>
+        <Icon name="map-marker-outline" color={colors.text} size={20} />
+        <TextInput
+          value={city}
+          onChangeText={setCity}
+          placeholderTextColor="#666666"
+          autoCorrect={false}
+          style={[
+            styles.textInput,
+            {
+              color: colors.text,
+            },
+          ]}
+        />
+      </View>
+
+      <FlatList
+        horizontal={true}
+        data={DATA}
+        style={{ marginTop: "5%", marginBottom: "5%" }}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: SPACING,
+        }}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity activeOpacity={0.9}>
+            <UploadImageList
+              list_num={index}
+              style={{
+                width: THUMB_SIZE,
+                height: THUMB_SIZE,
+                marginRight: SPACING,
+                borderRadius: 16,
+              }}
+            />
+          </TouchableOpacity>
+        )}
+      />
+
+      <TouchableOpacity
+        style={styles.commandButton}
+        onPress={() => sumbitProfile()}
+      >
+        <Text style={styles.panelButtonTitle}>Update Profile</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
 };
 
 export default ProfileScreen;
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: "#fff",
-		padding: "0%",
-	},
+  container: {
+    backgroundColor: "#fff",
+    padding: "0%",
+  },
 
-	commandButton: {
-		padding: 15,
-		borderRadius: 10,
-		backgroundColor: "blue",
-		alignItems: "center",
-		marginTop: 10,
-	},
+  commandButton: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: "blue",
+    alignItems: "center",
+    marginTop: 10,
+  },
 
-	panelButtonTitle: {
-		fontSize: 17,
-		fontWeight: "bold",
-		color: "white",
-	},
-	action: {
-		flexDirection: "row",
-		marginTop: 10,
-		marginBottom: 10,
-		paddingVertical: 10,
-		backgroundColor: "white",
-	},
-	actionError: {
-		flexDirection: "row",
-		marginTop: 10,
-		borderBottomWidth: 1,
-		borderBottomColor: "#A9F5FF",
-		paddingBottom: 5,
-	},
-	textInput: {
-		flex: 2,
-		marginTop: Platform.OS === "ios" ? 0 : -12,
-		marginLeft: 20,
-		marginRight: 20,
-		color: "black",
-		backgroundColor: "white",
-	},
+  panelButtonTitle: {
+    fontSize: 17,
+    fontWeight: "bold",
+    color: "white",
+  },
+  action: {
+    flexDirection: "row",
+    marginTop: 10,
+    marginBottom: 10,
+    paddingVertical: 10,
+    backgroundColor: "white",
+  },
+  actionError: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#A9F5FF",
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 2,
+    marginTop: Platform.OS === "ios" ? 0 : -12,
+    marginLeft: 20,
+    marginRight: 20,
+    color: "black",
+    backgroundColor: "white",
+  },
 });
--- src/views/UploadImageList.js
+++ src/views/UploadImageList.js
@@ -1,96 +1,107 @@
 import React, { useState, useEffect } from "react";
 import {
-	Image,
-	View,
-	Platform,
-	TouchableOpacity,
-	Text,
-	StyleSheet,
+  Image,
+  View,
+  Platform,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
-import { getDatabase, ref, onValue, set } from "firebase/database";
+import { getDatabase, onValue, set } from "firebase/database";
 // Import the functions you need from the SDKs you need
 import { app } from "./FirebaseInitialize";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 import { getFirestore, setDoc, doc, getDoc } from "firebase/firestore";
 
 const database = getDatabase(app);
 const firestore = getFirestore(app);
+const storage = getStorage(app);
 
 export default function UploadImageList(props) {
-	console.log(props.list_num);
-	const docRef = doc(firestore, "users", "DdRPo2lJfFbBcqkzAhXz");
-	getDoc(docRef)
-		.then((doc) => {
-			if (doc.exists) {
-				let data = doc.data();
-				setImage(data.Images[props.list_num]);
-			} else {
-				console.log("No such document");
-			}
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-	const [image, setImage] = useState(null);
-	const addImage = async () => {
-		let image_to_add = await ImagePicker.launchImageLibraryAsync({
-			mediaTypes: ImagePicker.MediaTypeOptions.Images,
-			allowsEditing: true,
-			aspect: [4, 3],
-			quality: 1,
-		});
+  console.log(props.list_num);
+  const docRef = doc(firestore, "users", "DdRPo2lJfFbBcqkzAhXz");
+  getDoc(docRef)
+    .then((doc) => {
+      if (doc.exists) {
+        let data = doc.data();
+        setImage(data.Images[props.list_num]);
+      } else {
+        console.log("No such document");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-		if (!image_to_add.cancelled) {
-			setImage(image_to_add.uri);
-		}
-	};
+  const [image, setImage] = useState(null);
+  const addImage = async () => {
+    let image_to_add = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
-	return (
-		<View style={imageUploaderStyles.container}>
-			{image && (
-				<Image
-					source={{ uri: image }}
-					style={{ width: "100%", height: "100%" }}
-				/>
-			)}
+    let imageName = "images/connecto" + (props.list_num + 1) + ".jpg";
+    console.log("updating", imageName);
+    let reference = ref(storage, imageName);
+    var message = await (await fetch(image_to_add.uri)).blob();
 
-			<View style={imageUploaderStyles.uploadBtnContainer}>
-				<TouchableOpacity
-					onPress={addImage}
-					style={imageUploaderStyles.uploadBtn}
-				>
-					<Text>{image ? "Edit" : "Upload"} </Text>
-					<AntDesign name="camera" size={20} color="black" />
-				</TouchableOpacity>
-			</View>
-		</View>
-	);
+    await uploadBytes(reference, message, { contentType: "image/jpeg" }).then(
+      (snapshot) => {
+        console.log("Uploaded a blob or file!");
+        setImage(image_to_add.uri);
+      }
+    );
+  };
+
+  return (
+    <View style={imageUploaderStyles.container}>
+      {image && (
+        <Image
+          source={{ uri: image }}
+          style={{ width: "100%", height: "100%" }}
+        />
+      )}
+
+      <View style={imageUploaderStyles.uploadBtnContainer}>
+        <TouchableOpacity
+          onPress={addImage}
+          style={imageUploaderStyles.uploadBtn}
+        >
+          <Text>{image ? "Edit" : "Upload"} </Text>
+          <AntDesign name="camera" size={20} color="black" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const imageUploaderStyles = StyleSheet.create({
-	container: {
-		elevation: 2,
-		height: 145,
-		width: 145,
-		marginRight: 10,
-		backgroundColor: "#efefef",
-		position: "relative",
-		borderRadius: 16,
-		overflow: "hidden",
-	},
-	uploadBtnContainer: {
-		opacity: 0.7,
-		position: "absolute",
-		right: 0,
-		bottom: 0,
-		backgroundColor: "lightgrey",
-		width: "100%",
-		height: "25%",
-	},
-	uploadBtn: {
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  container: {
+    elevation: 2,
+    height: 145,
+    width: 145,
+    marginRight: 10,
+    backgroundColor: "#efefef",
+    position: "relative",
+    borderRadius: 16,
+    overflow: "hidden",
+  },
+  uploadBtnContainer: {
+    opacity: 0.7,
+    position: "absolute",
+    right: 0,
+    bottom: 0,
+    backgroundColor: "lightgrey",
+    width: "100%",
+    height: "25%",
+  },
+  uploadBtn: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
