a1fc04500942cc35c51de46e3dd7dd14a93e93ee
aaronli39 <aaronli39@gmail.com>
2022-04-23 13:33:20 -0400

    api key

--- src/constants/DataConstants.js
+++ src/constants/DataConstants.js
@@ -1,7 +1,7 @@
 const CONSTANTS = {
 	serpApiP1: "https://serpapi.com/search.json?engine=google_events&q=",
 	serpApiP2:
-		"&hl=en&gl=us&api_key=5e968b2fd6cd27efee6d82224af6f45c571111b2e3850b07d2e042e84e780059",
+		"&hl=en&gl=us&api_key=7421c16ff7749280c437620191e336fbab9efde01e6379262a7b84ff51a47de8",
 };
 
 module.exports = { CONSTANTS };
